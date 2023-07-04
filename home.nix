a3f17ec0355de3a23a35fc2ffcfcf53acbf391a4
arifinoid <rohmadarifin@gmail.com>
2023-07-04 22:24:51 +0700

    initial commit on nixpkgs

---
+++ home.nix
@@ -0,0 +1,99 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "arifinoid";
+  home.homeDirectory = "/home/arifinoid";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    pkgs.curl
+    pkgs.tmux
+    pkgs.neovim
+    pkgs.neofetch
+    pkgs.ripgrep
+    pkgs.htop
+    pkgs.exa
+    pkgs.trash-cli
+    pkgs.openvpn
+    pkgs.openvpn3
+    pkgs.alacritty
+    pkgs.starship
+    pkgs.nodejs_20
+    pkgs.go
+    pkgs.gcc9
+    pkgs.libstdcxx5
+    pkgs.python39
+    pkgs.git
+    pkgs.lazygit
+    pkgs.fish
+    pkgs.nerdfonts
+    pkgs.rustc
+    pkgs.cargo
+    pkgs.zig
+    pkgs.fishPlugins.z
+    pkgs.fishPlugins.bass
+
+
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/arifinoid/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  programs.fish.enable = true;
+}
